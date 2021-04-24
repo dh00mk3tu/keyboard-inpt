e8f6f98ddaddb8bf5540bd52aa39f0ffcf2740ee
dh00mk3tu <cyberhybird@hotmail.com>
2021-04-24 18:53:39 +0530

    hehe

--- bruhh.py
+++ bruhh.py
@@ -5,10 +5,13 @@ keyboard = Controller()
 
 alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def type_string_with_delay():
-    for character in alphabet_list:
-        keyboard.type(random.choice(alphabet_list))
-        delay = random.uniform(0, 2)
-        time.sleep(1.7)  
 
+def type_string_with_delay():
+    while True: 
+        for character in alphabet_list:
+            keyboard.type(random.choice(alphabet_list))
+            delay = randomkb.uniform(0, 2)
+            time.sleep(.3)
+        
+        
 type_string_with_delay()
