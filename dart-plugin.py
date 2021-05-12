5f62d6d78a82b57626c2b70aa656d1f84915ea53
dh00mk3tu <cyberhybird@hotmail.com>
2021-05-12 10:52:52 +0530

    changes

--- dart-plugin.py
+++ dart-plugin.py
@@ -1,10 +1,12 @@
 import pyautogui
 import math
 import time
+import random 
 
 while True:
 # Radius 
     R = 500
+    n = int(0.8)
     # measuring screen size
     (x,y) = pyautogui.size()
     # locating center of the screen 
@@ -13,7 +15,7 @@ while True:
     pyautogui.moveTo(X+R,Y)
 
     for i in range(360):
-        time.sleep(.1)
+        time.sleep(random.randint(n, 2))
         # setting pace with a modulus 
         if i%6==0:
             pyautogui.moveTo(X+R*math.cos(math.radians(i)),Y+R*math.sin(math.radians(i)))
