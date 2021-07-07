63aaace89aac9e70d12e59dc544991a3d0c2920a
dh00mk3tu <cyberhybird@hotmail.com>
2021-07-07 13:48:10 +0530

    hehe

--- dart-plugin.py
+++ dart-plugin.py
@@ -8,7 +8,7 @@ while True:
 # Radius 
     R = 500
     n = int(0.1)
-    m = int(0.9)
+    m = int(0.8)
     # measuring screen size
     (x,y) = pyautogui.size()
     # locating center of the screen 
@@ -20,7 +20,7 @@ while True:
     
     for i in range(360):
         time.sleep(random.randint(n, m))
-        time.sleep(random.uniform(0.3, 0.8))
+        time.sleep(random.uniform(0.1, 0.4))
         # time.sleep(.5)
         # setting pace with a modulus 
         if i%6==0:
